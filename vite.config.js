d4d88a2f39c7e9e0e66775985fef99c89a602de0
Ali Torabi <alithema@gmail.com>
2025-08-19 10:23:39 -0600

    Update GitHub Pages configuration with correct repository name

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: process.env.NODE_ENV === 'production' ? '/[REPO_NAME]/' : '/',
+  base: process.env.NODE_ENV === 'production' ? '/LabWebsite-Frontend/' : '/',
   server: {
     proxy: {
       '/api': 'http://localhost:8000',
