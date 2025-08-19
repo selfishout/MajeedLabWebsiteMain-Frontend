24e6ab71c8f94f76f5bf3483e7d639a0e45eb1f0
Ali Torabi <alithema@gmail.com>
2025-08-19 12:47:51 -0600

    Switch to HashRouter for better GitHub Pages compatibility

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom'
+import { HashRouter as Router, Routes, Route, useLocation } from 'react-router-dom'
 import './App.css'
 import About from './pages/About/About'
 import Home from './pages/Home/Home'
